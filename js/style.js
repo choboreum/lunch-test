5ef46b16c55943d1d68b00e29898286c6d6180fb
boreumHome <choboreum@naver.com>
2022-07-05 21:39:06 +0900

    개발 1차 덮기

--- js/style.js
+++ js/style.js
@@ -1,3 +1,33 @@
+var usrnic = document.getElementById('userNickName');
+var usrage = document.getElementById('userAge');
+var usrgen = document.getElementById('userNickName');
+
+//사용자 이름 바뀌었을때 이벤트
+usrnic.addEventListener('change',function(){
+  let x = document.getElementsByClassName("chaNickName")[0];
+  x.innerText = usrnic.value ;
+  window.localStorage.setItem('name',usrnic.value);
+})
+
+//나이 바뀌었을 때 이벤트
+usrage.addEventListener('change',function(){
+  //나이값의 크기비교
+  //크기별로 이미지 변경
+})
+
+//라디오는 아이디를 어떻게 먹여서 변경될때마다 액션을 줄지 확인해보기
+
+///////////////////////////////////////////////////////////////////
+
+
+//3페이지넘어갈때 뿌려주
+
+let x = document.getElementsByClassName("chaNickName")[0];
+x.innerText = window.localStorage.getItem('name');
+
+///////////////////////////////////////////////////////////////////
+
+
 function check(){
     $("a[data-role=submit]").unbind('click').on('click', function() {
         if($('input[name="name"]').val().trim() == ''){
