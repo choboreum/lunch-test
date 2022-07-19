f7e2ba84fb907cb32c4f33a3b49c297672a5ad80
boreumHome <choboreum@naver.com>
2022-07-19 23:09:22 +0900

    ~
    (*표시는 수정 해야 할 부분)
    2page
    - 나이 0~99까지 설정 완료
    - 나이별 (0~20/ 21~40/ 41~) css 완료
    - 나이 입력시 스크립트 부분
    * 스크립트 오류 많음ㅠㅠ 보완하기

--- js/style.js
+++ js/style.js
@@ -27,7 +27,84 @@ x.innerText = window.localStorage.getItem('name');
 
 ///////////////////////////////////////////////////////////////////
 
+/* 성별 검사 */
+function findGender(){
+    var gender_value = $('input:radio[name=gender]:checked').val();
+    //console.log(gender_value);
+    var female = $('.bun-r, .bun-l')
+    var clothes = $('.clothes')
 
+    if(gender_value == 'male') {
+        female.addClass('none');
+        clothes.css('border-bottom', '95px solid #009688');
+    } else {
+        female.removeClass('none');
+        clothes.css('border-bottom', '95px solid #f25437');
+    }
+}
+
+/* 나이, 2글자 숫자만 가능하게*/
+var userAge = document.getElementById('userAge');
+
+userAge.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+        || (e.keyCode > 47 && e.keyCode < 58) 
+        || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+$('input[type=number][maxlength]').on('input', function(ev) {
+    var $this = $(this);
+    var maxlength = $this.attr('maxlength');
+    var value = $this.val();
+    if (value && value.length >= maxlength) {
+        $this.val(value.substr(0, maxlength));
+    }
+});
+
+/* 연령별 구분 및 css처리 */
+setTimeout(()=>{
+    var chi = document.getElementById("childBag");
+    var chet = chi.getContext("2d");
+    chet.beginPath();
+    chet.strokeStyle="#000";
+    chet.lineWidth=10;
+    chet.arc(0, 20, 100, 0,Math.PI,false);
+    chet.stroke();
+
+    var chi = document.getElementById("adultBag");
+    var chet = chi.getContext("2d");
+    chet.beginPath();
+    chet.strokeStyle="#000";
+    chet.lineWidth=10;
+    chet.arc(140 , 140 , 40 , 0, Math.PI, true);
+    chet.stroke();
+}, 500)
+
+function checkUserAge (obj) {
+    var userAgeValue = document.getElementById('userAge').value;
+    if (userAgeValue >= 0 || userAgeValue <= 20 ){
+        console.log('아동');
+        $('.bag .child').addClass('on');
+        $('.bag .adult').removeClass('on');
+        $('.headbg, .hair-r, .hair-l, .bun-r, .bun-l').css('background','#714935');
+
+    } else if (userAgeValue > 21 || userAgeValue <= 40 ) {
+        console.log('성인');
+        $('.bag .child').removeClass('on');
+        $('.bag .adult').addClass('on');
+        $('.headbg, .hair-r, .hair-l, .bun-r, .bun-l').css('background','#3d3435');
+
+    } else if (userAgeValue > 41) {
+        console.log('중년');
+        $('.bag .child').removeClass('on');
+        $('.bag .adult').addClass('on');
+        $('.headbg, .hair-r, .hair-l, .bun-r, .bun-l').css('background','#646464');
+    }
+}
+
+/* input 검사 */
 function check(){
     $("a[data-role=submit]").unbind('click').on('click', function() {
         if($('input[name="name"]').val().trim() == ''){
@@ -52,21 +129,7 @@ function check(){
     })
 };
 
-function findGender(){
-    var gender_value = $('input:radio[name=gender]:checked').val();
-    //console.log(gender_value);
-    var female = $('.bun-r, .bun-l')
-    var clothes = $('.clothes')
-
-    if(gender_value == 'male') {
-        female.addClass('none');
-        clothes.css('border-bottom', '95px solid #009688');
-    } else {
-        female.removeClass('none');
-        clothes.css('border-bottom', '95px solid #f25437');
-    }
-}
-
+/* 질문페이지 효과 */
 function nextPage(){
     $("button").on('click', function() {
         $(this).parent('section').removeClass('on')
